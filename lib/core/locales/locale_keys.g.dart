65cfcf4aef38fee8b5e45878715a383b88348b08
Malintha Nilhan Samarasinghe <86799359+MalinthaSamarasinghe@users.noreply.github.com>
2024-05-27 12:26:22 +0530

    Initialization of Localization

---
+++ lib/core/locales/locale_keys.g.dart
@@ -0,0 +1,8 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const features_login_title = 'features.login.title';
+  static const features_login = 'features.login';
+  static const features = 'features';
+
+}
